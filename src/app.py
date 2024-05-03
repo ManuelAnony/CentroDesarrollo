f539edf9de8d979c1095143a3ecce4f95dc8ca44
Manuuel Felipe Forero <47534318+ManuelAnony@users.noreply.github.com>
2024-05-03 01:27:27 -0500

    puesta en marcha

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId
 from config import *
 import re 
+from flask_cors import CORS
 import smtplib
 import random
 from email.mime.multipart import MIMEMultipart
@@ -667,4 +668,5 @@ def notificar_equipo(proyecto_id):
     return render_template('notificar_equipo.html', proyecto_id=proyecto_id)
 
 if __name__ == '__main__':
+    CORS(app)
     app.run(debug=True)
